a6c05f4d13f07d19780c3ef67127bcde204a0d84
Megagig <megagigsoftwaresolutions.com>
2024-07-01 00:02:18 +0100

    Solve cors origin errros, import bodyParser and cookie parsar

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// solving the cors origin errors
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+// bodyParser
+app.use(bodyParser.json());
+
+//cookie parser
+app.use(cookieParser);
 
 // Importing the routes
 app.use('/api', require('./routes/authRoutes'));
