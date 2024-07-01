e4e94f8f35d16e2b85c799f61accdf2a6647c24c
Megagig <megagigsoftwaresolutions.com>
2024-07-01 21:10:33 +0100

    Authentication with jsonwebtoken

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,7 +1,7 @@
 const adminModel = require('../models/adminModel');
 const { responseReturn } = require('../utilities/response');
 const bcrpty = require('bcrypt');
-
+const { createToken } = require('../utilities/tokenCreate');
 class authControllers {
   admin_login = async (req, res) => {
     const { email, password } = req.body;
@@ -10,7 +10,22 @@ class authControllers {
       // console.log(admin);
       if (admin) {
         const match = await bcrpty.compare(password, admin.password);
-        console.log(match);
+        // console.log(match);
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie('accessToken', token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, {
+            token,
+            message: 'Admin logged in sucessfully',
+          });
+        } else {
+          responseReturn(res, 404, { error: 'Password is Incorrect' });
+        }
       } else {
         responseReturn(res, 404, { error: 'Email not Found' });
       }
